787f946019c79064d0296bbf1cbd87af26474b03
student <student@DEV2-21>
2019-02-21 12:05:56 +0900

    弾の優先順位をつけて見えるようにした

--- TeamPlanet/TeamPlanet/Hero.cpp
+++ TeamPlanet/TeamPlanet/Hero.cpp
@@ -53,6 +53,7 @@ void CHero::Action()
 		{
 			//弾丸オブジェクト作成
 			CBullet* bullet = new CBullet(m_x + 120.0f, m_y + 30.0f);
+			bullet->m_priority = 90;
 			TaskSystem::InsertObj(bullet);
 
 			m_f = false;
