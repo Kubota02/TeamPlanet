5c7a44dda89b5518a014ff1ddcb42c7bbfee6aa4
GA3 <GA3@DEV1-9>
2019-04-22 11:29:57 +0900

    敵データ管理用

---
+++ TeamPlanet/TeamPlanet/EnemyData.cpp
@@ -0,0 +1,123 @@
+//STLデバック機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+#include <stdio.h>
+#include <locale.h>
+
+#include "EnemyData.h"
+
+using namespace std;
+
+#define SAVE_NAME (L"enemy_data")
+
+void* EnemyData::m_data;		//敵情報
+int   EnemyData::m_data_size;	//敵情報サイズ
+ENEMYDATA e_data[ENEMY_NUM];
+
+void* EnemyData::GetData()
+{
+	return m_data;
+}
+
+void EnemyData::Init(void *data, int size)
+{
+	setlocale(LC_CTYPE, "jpn");
+	m_data = data;
+	m_data_size = size;
+}
+
+void EnemyData::Delete()
+{
+	m_data = nullptr;
+	m_data_size = 0;
+}
+
+void EnemyData::Open()
+{
+	FILE *fp;
+	_wfopen_s(&fp, SAVE_NAME, L"r");
+
+	if (fp == nullptr)
+	{
+		memset(m_data, 0x00, m_data_size);
+		return;
+	}
+
+	fread(m_data, m_data_size, 1, fp);
+
+	fclose(fp);
+}
+
+void EnemyData::Save()
+{
+	FILE* fp;
+	_wfopen_s(&fp, SAVE_NAME, L"w");
+
+	if (fp == nullptr)
+		return;
+
+	fwrite(m_data, m_data_size, 1, fp);
+
+	fclose(fp);
+}
+
+unique_ptr<wchar_t> EnemyData::ExternalDataOpen(wchar_t* file_name, int* size)
+{
+	FILE *fp;
+	
+	char buf[100];
+	int c;
+	int col = 1;
+	int row = 0;
+
+	memset(buf, 0, sizeof(buf));
+	_wfopen_s(&fp,file_name, L"r");
+
+	//while (fgetc(fp) != '\n'); //ヘッダ読み飛ばし
+	//while (fgetc(fp) != '\n' && !feof(fp));
+
+	while (1)
+	{
+		while (1)
+		{
+			c = fgetc(fp);
+
+			//末尾ならループを抜ける。
+			if (c == EOF)
+				return nullptr;
+
+			//カンマか改行でなければ、文字としてつなげる
+			if (c != ','&& c != '\n')
+				strcat_s(buf, (const char*)&c);
+			//カンマか改行ならループ抜ける。
+			else
+				break;
+		}
+
+		//ここに来たということは、1セル分の文字列が出来上がったということ
+		switch (col)
+		{
+		//1列目は敵種類を表す。atoi関数で数値として代入。
+		case 1:e_data[row].enemy_type = atoi(buf); break;
+		//2列目は弾種類。以降省略。
+		case 2:e_data[row].in_time = atoi(buf); break;
+		case 3:e_data[row].x = atoi(buf); break;
+		case 4:e_data[row].y = atoi(buf); break;
+		case 5:e_data[row].enemy_speed = atoi(buf); break;
+		case 6:e_data[row].hp = atoi(buf); break;
+		case 7:e_data[row].item = atoi(buf); break;
+		}
+		//バッファを初期化
+		memset(buf, 0, sizeof(buf));
+		//列数を足す
+		++col;
+
+		//もし読み込んだ文字が改行だったら列数を初期化して行数を増やす
+		if (c == '\n')
+		{
+			col = 1;
+			++row;
+		}
+	}
+}
---
+++ TeamPlanet/TeamPlanet/EnemyData.h
@@ -0,0 +1,42 @@
+#pragma once
+//使用するヘッダ
+#include "Draw2DPolygon.h"
+#include "TaskSystem.h"
+#include "Collision.h"
+
+#pragma once
+
+#include <memory>
+using namespace std;
+
+//ユーザー・セーブ・シーン間受け渡しデータクラス
+typedef class EnemyData
+{
+public:
+	static void Init(void* data, int size);
+	static void Delete();
+	static void Open();
+	static void Save();
+	static void* GetData();
+
+	//ユーザーデータ以外の外部データ読み込み
+	//全データ(文字列情報)を取得したメモリが返される
+	//テキストエディタでUnicode指定
+	static unique_ptr<wchar_t> ExternalDataOpen(wchar_t* file_name, int* size);
+private:
+	static void* m_data;
+	static int   m_data_size;
+}Save;
+
+#define ENEMY_NUM 5
+
+struct ENEMYDATA
+{
+	int enemy_type;	//敵の種類
+	int in_time;	//出現時間
+	int x;			//X座標
+	int y;			//Y座標
+	int enemy_speed;//敵のスピード
+	int hp;			//HP
+	int item;		//アイテム
+};
