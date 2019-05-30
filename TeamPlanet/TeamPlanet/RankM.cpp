4d157ccf179b8449d0bdee34cb1c9988f18b7146
student <student@DEV2-21>
2019-05-30 11:56:53 +0900

    2番

--- TeamPlanet/TeamPlanet/RankM.cpp
+++ TeamPlanet/TeamPlanet/RankM.cpp
@@ -24,53 +24,11 @@ CRankM::~CRankM()
 
 void CRankM::Action()
 {
-	/*if (m_ani_time != 0)
-	{
 
-		m_ani_time++;
-	}
-
-	if (total >= 1000)
-	{
-		if (g_key_flag)
-		{
-			m_ani_time++;
-			g_key_flag = false;
-		}
-	}
-	else
-	{
-		g_key_flag = true;
-	}
-
-	g_key_flag = true;*/
 }
 
 void CRankM::Draw()
 {
-	/*if (m_ani_time >= 100)
-	{
-		if (total >= 200 && total < 400)
-		{
-			Draw::Draw2D(46, m_x, m_y);
-		}
-		else if (total >= 400 && total < 600)
-		{
-			Draw::Draw2D(47, m_x, m_y);
-		}
-		else if (total >= 600 && total < 800)
-		{
-			Draw::Draw2D(48, m_x, m_y);
-		}
-		else if (total >= 800 && total < 1000)
-		{
-			Draw::Draw2D(49, m_x, m_y);
-		}
-		else if (total >= 1000)
-		{
-			Draw::Draw2D(50, m_x, m_y);
-		}
-	}*/
 	if (total >= 200 && total < 400)
 	{
 		Draw::Draw2D(46, m_x, m_y);
